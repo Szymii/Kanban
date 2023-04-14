13a690cb7c9f4a7776f1f13b52228c5a9334187a
Szymii <szymimelzer9@gmail.com>
2023-04-14 16:25:38 +0200

    database seed for dev env

---
+++ prisma/seed.ts
@@ -0,0 +1,40 @@
+// npx prisma db seed
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.upsert({
+    where: { email: "test@example.com" },
+    update: {},
+    create: {
+      email: "test@example.com",
+      name: "Tester",
+      password: "", //
+      emailVerified: null,
+      image: null,
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: "admin@example.com" },
+    update: {},
+    create: {
+      email: "admin@example.com",
+      name: "Admin",
+      password: "", //
+      emailVerified: null,
+      image: null,
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
